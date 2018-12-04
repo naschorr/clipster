ebc8aa5b9d1429afeaf9039a71a00ce6918880b8
Nick Schorr <naschorr@gmail.com>
2018-12-03 20:22:04 -0500

    Reorganized on_command_error method to be a bit cleaner

--- code/clipster.py
+++ code/clipster.py
@@ -227,20 +227,7 @@ class Clipster:
                     "Sorry <@{}>, **{}{}** isn't a valid command.".format(ctx.message.author.id, ctx.prefix, ctx.invoked_with)
                 ]
 
-                ## Build a message string that we can compare with.
-                try:
-                    message = ctx.message.content[len(self.activation_string):]
-                except TypeError:
-                    message = ctx.message.content
-
-                ## Find the most similar command
-                most_similar_command = (None, 0)
-                for key in self.bot.commands.keys():
-                    distance = StringSimilarity.similarity(key, message)
-                    if (distance > most_similar_command[1]):
-                        most_similar_command = (key, distance)
-
-                ## Calculate the output to give to the user
+                most_similar_command = self.find_most_similar_command(ctx.message.content)
                 if (most_similar_command[1] > self.invalid_command_minimum_similarity):
                     help_text_chunks.append("Did you mean **{}{}**?".format(self.activation_string, most_similar_command[0]))
                 else:
@@ -276,6 +263,23 @@ class Clipster:
     def register_module(self, cls, is_cog, *init_args, **init_kwargs):
         self.module_manager.register(cls, is_cog, *init_args, **init_kwargs)
 
+
+    ## Finds the most similar command to the supplied one
+    def find_most_similar_command(self, command):
+        ## Build a message string that we can compare with.
+        try:
+            message = command[len(self.activation_string):]
+        except TypeError:
+            message = command
+
+        ## Find the most similar command
+        most_similar_command = (None, 0)
+        for key in self.bot.commands.keys():
+            distance = StringSimilarity.similarity(key, message)
+            if (distance > most_similar_command[1]):
+                most_similar_command = (key, distance)
+
+        return most_similar_command
 
     ## Run the bot
     def run(self):
